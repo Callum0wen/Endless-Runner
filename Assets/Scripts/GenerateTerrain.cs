a3e23a007dc8ffd2c015448492b21e4b7302a03b
Callum Owen <34078115+Callum0wen@users.noreply.github.com>
2019-02-26 15:10:18 +0000

    Started SmartPlane alterative

---
+++ Assets/Scripts/GenerateTerrain.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/**
+ * Code from https://www.youtube.com/watch?v=dycHQFEz8VI
+**/
+public class GenerateTerrain : MonoBehaviour
+{
+	int heightScale = 4;
+	float detailScale = 10f;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+		for(int v = 0; v < vertices.Length; v++)
+		{
+			vertices[v].y = (Mathf.PerlinNoise((vertices[v].z + this.transform.position.z) / detailScale,
+												(vertices[v].z + this.transform.position.z) / detailScale) * heightScale)
+												- (this.transform.position.z + (v/11));
+		}
+
+		mesh.vertices = vertices;
+		mesh.RecalculateBounds();
+		mesh.RecalculateNormals();
+		this.gameObject.AddComponent<MeshCollider>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -8,12 +8,18 @@ public class MeshGenerator : MonoBehaviour
     Vector3[] vertices;
     int[] triangles;
 
-    public int xSize = 20;
+    public int xSize = 40;
     public int zSize = 20;
+	public float detailScale = .1f;
+	public float heightScale = 6f;
+	public float slope = .2f;
+	public Vector2 startOffset;
 
     // Start is called before the first frame update
     void Start()
     {
+		startOffset = new Vector2(Random.Range(0f, 256f), Random.Range(0f, 256f));
+
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
 
@@ -30,17 +36,18 @@ public class MeshGenerator : MonoBehaviour
         {
             for (int x = 0; x <= xSize; x++)
             {
-				float y = (Mathf.PerlinNoise(x * .3f, x * .3f) * 2f) - (x * .3f);
+				float y = Mathf.PerlinNoise((x + startOffset.x) * detailScale, (x + startOffset.x) * detailScale) * heightScale;
+				y = y - (x * slope);
 				vertices[i] = new Vector3(x, y, z);
                 i++;
             }
         }
 
 		triangles = new int[xSize * zSize * 6];
-
 		int vert = 0;
 		int tris = 0;
-
+		//Loop assigns the points of two triangles to make a square and loops through all the squares.
+		//i.e. 1 square made of 2 triangles, each triangle needs 3 points.
 		for (int z = 0; z < zSize; z++)
 		{
 			for (int x = 0; x < xSize; x++)
