363d85d5a87e8e0d8d2fbcbebc17477657cbbc53
Callum Owen <34078115+Callum0wen@users.noreply.github.com>
2019-02-28 03:39:42 +0000

    fixed player not being able to go lower than 0.5y

--- Assets/Scripts/GenerateTerrain.cs
+++ Assets/Scripts/GenerateTerrain.cs
@@ -8,6 +8,7 @@ public class GenerateTerrain : MonoBehaviour
 {
 	int heightScale = 4;
 	float detailScale = 10f;
+	int slope = 3;
 
 	// Start is called before the first frame update
 	void Start()
@@ -16,11 +17,12 @@ public class GenerateTerrain : MonoBehaviour
 		//noise value, relative to the detail (i.e. the smoothness), and the height (actual height of the noise).
 		Mesh mesh = this.GetComponent<MeshFilter>().mesh;
 		Vector3[] vertices = mesh.vertices;
+		this.transform.Translate(0, -this.transform.position.z / slope, 0);
 		for(int v = 0; v < vertices.Length; v++)
 		{
 			vertices[v].y = (Mathf.PerlinNoise((vertices[v].z + this.transform.position.z) / detailScale,
 												(vertices[v].z + this.transform.position.z) / detailScale) * heightScale)
-												- (vertices[v].z + this.transform.position.z)/3;
+												- (vertices[v].z / slope);
 		}
 
 		//Reasigns the vertices back to the plane and recalulates so that it renders correctly.
