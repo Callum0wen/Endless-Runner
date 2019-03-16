f98ecf8f366150dc38aa69114d04658ee06bc36c
Callum Owen <34078115+Callum0wen@users.noreply.github.com>
2019-03-16 01:11:34 +0000

    Adding Jump script

---
+++ Assets/Scripts/LeanAndJump.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class LeanAndJump : MonoBehaviour
+{
+	public Rigidbody rb;
+	public float jumpForce = 200f;
+
+	void ResetTensor()
+	{
+		GetComponent<Rigidbody>().inertiaTensorRotation = Quaternion.identity;
+	}
+
+	void FixedUpdate()
+    {
+		if (Input.GetKey("space"))
+		{
+			rb.AddForce(0, jumpForce, 0);
+		}
+	}
+}
