f3dd0daade5d2b6d044b8881cf63a9d1660f26cc
Callum Owen <34078115+Callum0wen@users.noreply.github.com>
2019-03-19 02:05:25 +0000

    Got ragdol on man

---
+++ Assets/ManNoCollide.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ManNoCollide : MonoBehaviour
+{
+	public GameObject bike;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		Physics.IgnoreCollision(this.GetComponent<Collider>(), bike.GetComponent<Collider>());
+    }
+
+}
